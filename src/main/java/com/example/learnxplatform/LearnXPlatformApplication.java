f7e1e5eedd390b799995776c0f803e657d5547d8
Paul Jideani <pcijideani@gmail.com>
2024-05-11 17:54:51 +0200

    modified application properties

---
+++ src/main/java/com/example/learnxplatform/LearnXPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnxplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnXPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnXPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnxplatform/LearnXPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnxplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnXPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
