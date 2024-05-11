392dbeccc6cfbc61f0fa78f8bcb7451e9d9cdc2b
Paul Jideani <pcijideani@gmail.com>
2024-05-11 20:41:26 +0200

    TutorAvailabilityController

---
+++ src/main/java/com/example/learnxplatform/controller/TutorAvailabilityController.java
@@ -0,0 +1,44 @@
+package com.example.learnxplatform.controller;
+
+import com.example.learnxplatform.dto.TutorAvailabilityRequest;
+import com.example.learnxplatform.entity.TutorAvailability;
+import com.example.learnxplatform.service.TutorAvailabilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tutor-availabilities")
+@CrossOrigin(origins = "http://localhost:3000")
+public class TutorAvailabilityController {
+
+    @Autowired
+    private TutorAvailabilityService tutorAvailabilityService;
+
+    @GetMapping
+    public ResponseEntity<List<TutorAvailability>> getAllTutorAvailabilities() {
+        List<TutorAvailability> tutorAvailabilities = tutorAvailabilityService.getAllTutorAvailabilities();
+        return ResponseEntity.ok(tutorAvailabilities);
+    }
+
+    @PostMapping
+    public ResponseEntity<TutorAvailability> createTutorAvailability(@RequestBody TutorAvailabilityRequest request) {
+        TutorAvailability createdAvailability = tutorAvailabilityService.createTutorAvailability(request);
+        return new ResponseEntity<>(createdAvailability, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/by-tutor/{tutorId}")
+    public ResponseEntity<List<TutorAvailability>> getTutorAvailabilityByTutor(@PathVariable Long tutorId) {
+        List<TutorAvailability> tutorAvailabilities = tutorAvailabilityService.getTutorAvailabilityByTutor(tutorId);
+        return ResponseEntity.ok(tutorAvailabilities);
+    }
+
+    @DeleteMapping("/{requestId}")
+    public ResponseEntity<Void> deleteTutorAvailability(@PathVariable Long requestId) {
+        tutorAvailabilityService.deleteTutorAvailability(requestId);
+        return ResponseEntity.ok().build();
+    }
+}
